41201231d503e8d12acd23b8fc0db8fec7871448
Kojo-Brown <100282824+Kojo-Brown@users.noreply.github.com>
2025-07-24 23:02:23 -0400

    fix: add prod url

--- src/index.ts
+++ src/index.ts
@@ -59,6 +59,7 @@ const io = new Server(server, {
       "http://localhost:3000",
       "http://localhost:5173",
       "http://localhost:5174",
+      "https://duonest-frontend.vercel.app",
     ],
     methods: ["GET", "POST"],
   },
